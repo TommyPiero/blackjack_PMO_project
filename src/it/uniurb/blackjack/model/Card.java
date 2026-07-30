31a4abd5f46aefd712691e91d69218b5297e784c
TommyPiero <tommypierelli@gmail.com>
2026-07-30 11:42:20 +0200

    feat: created the Card interface and the enum Suit for cards

---
+++ src/it/uniurb/blackjack/model/Card.java
@@ -0,0 +1,14 @@
+package it.uniurb.blackjack.model;
+
+public interface Card {
+	// declaration of methods
+	
+	// getter method for the nominal value of the card
+	public int getNominalValue();
+	
+	// getter method for the suit of the card
+	public Suit getSuit();
+	
+	// getter method for the BJ value of the card
+	public int getBlackjackValue();
+}
---
+++ src/it/uniurb/blackjack/model/Suit.java
@@ -0,0 +1,9 @@
+package it.uniurb.blackjack.model;
+
+// enum for the suit of cards
+public enum Suit {
+	HEARTS,
+	DIAMONDS,
+	CLUBS,
+	SPADES;
+}
